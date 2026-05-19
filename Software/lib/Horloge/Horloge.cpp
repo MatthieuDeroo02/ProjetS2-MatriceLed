3299284acdb9ed6f20c71f3f24e608e762cdfed7
clem <Clement.pii@etu.univ-smb.fr>
2026-05-19 18:29:34 +0200

    fix BP

--- Software/lib/Horloge/Horloge.cpp
+++ Software/lib/Horloge/Horloge.cpp
@@ -14,6 +14,7 @@
 void clock::InitRTC(){
     Wire.begin();
     TinyRtc.begin();
+    InitPort();
 }
 void clock::InitPort(){
     DDRD |= Masque_PD3 |Masque_PD2;
@@ -41,23 +42,30 @@ void clock::SetDates(){
 
 
 void clock::Afficher_Heures(){
+    InitRTC();
     if(BP1_Appuyer()){
         UpdateRTC();
         SetHeures();
-
+        Serial.println(times.hour);
     }
 }
 
 /*------BP---------*/
 bool clock::BP1_Appuyer(){
-    if(PORTD &= Masque_PD2){
+    if((PIND & Masque_PD2) == 0){
+        Serial.println("BP1");
         return true;
     }
-    return false;
+    else{
+        return false;
+    }
 }
 bool clock::BP2_Appuyer(){
-        if(PORTD &= Masque_PD3){
-        return true;
+        if((PIND & Masque_PD3) == 0){
+            Serial.println("BP2");
+            return true;
     }
-    return false;
+    else{
+        return false;
+    }
 }
