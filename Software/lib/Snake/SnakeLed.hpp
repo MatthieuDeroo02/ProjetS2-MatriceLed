0b5a40475d0a43c0751d6ae4700c5451731f823c
Clems <Clement.pili@etu.univ-smb.fr>
2026-05-21 12:55:48 +0200

    nex mode snake

---
+++ Software/lib/Snake/SnakeLed.hpp
@@ -0,0 +1,21 @@
+#ifndef _SNAKE_
+#define _SNAKE_
+
+#include <Arduino.h>
+
+#define START_X
+#define START_Y
+
+class snake {
+    public:
+
+
+    private:
+        
+
+
+};
+
+snake mySnake;
+
+#endif
