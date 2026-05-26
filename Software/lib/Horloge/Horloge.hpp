0c1753dd5cd218395d521db993eb11e6e42c3542
Clems <Clement.pili@etu.univ-smb.fr>
2026-05-26 14:15:43 +0200

    update snake sens

--- Software/lib/Horloge/Horloge.hpp
+++ Software/lib/Horloge/Horloge.hpp
@@ -48,6 +48,9 @@ public:
 
     void Init_Heures();
     T_Times GetTime();
+    
+    bool BP1_Appuyer();
+    bool BP2_Appuyer();
 
 private:
     void InitPort();
@@ -56,9 +59,6 @@ private:
 
     void SetHeures();
     void SetDates();
-
-    bool BP1_Appuyer();
-    bool BP2_Appuyer();
 
     uint8_t ConvNumAscii(uint8_t num);
 
